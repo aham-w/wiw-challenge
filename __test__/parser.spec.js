93e160e7b72dac10fdba2785a14854d2a1102613
aham-w <amithampal+wiw@gmail.com>
2023-11-30 21:05:06 -0500

    test: parser validation

--- __test__/parser.spec.js
+++ __test__/parser.spec.js
@@ -1,10 +1,20 @@
-const { describe, expect, it } = require('@jest/globals');
+const { describe, expect, test } = require('@jest/globals');
 const { parser } = require('../parser');
 const dataset = require('../__test__/data/dataset.json')
 
 describe('parser', () => {
-    it('parse dataset', () => {
+    test('parse dataset', () => {
         const output = parser(dataset);
         expect(output).toMatchSnapshot();
-    })
-})
+    });
+
+    test('invalid json formats', () => {
+        const ts = '2021-01-03T00:00:00.000Z';
+
+        expect(parser({})).toBe(null);
+        expect(parser([{ ShiftID: 1, EmployeeID: 1, StartTime: ts }])).toBe(null);
+        expect(parser([{ ShiftID: 1, EmployeeID: 1, EndTime: ts }])).toBe(null);
+        expect(parser([{ ShiftID: '', EmployeeID: 1, EndTime: ts, StartTime: ts }])).toBe(null);
+        expect(parser([{ ShiftID: 1, EmployeeID: true, EndTime: ts, StartTime: ts }])).toBe(null);
+    });
+});
